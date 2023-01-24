2c7a877987c896382684158050598ba4ccf01454
knutter1 <104777428+knutter1@users.noreply.github.com>
2023-01-24 16:38:09 +0100

    prototyp ohne warenkorb

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+filespath = __file__.rsplit("\\",1)[0] + "\\files\\"
 
 @app.route('/')
 def home():
@@ -15,9 +16,25 @@ def warenkorb():
 def about():
     return render_template("about.html", title="Über uns")
 
-@app.route('/produkt')
-def produkt():
+@app.route('/produktt')
+def produktt():
     return render_template("produkt.html", title="Detailansicht")
+
+@app.route('/produkt/<path:produktname>')
+def produkt(produktname):
+    file = open(filespath + produktname + ".txt", "r", encoding="UTF-8")
+    data = {}
+    for line in file.readlines():
+        keyword = line[:line.index(":")]
+        value = line[line.index(":")+1:]
+        data[keyword] = value
+        print(f"{keyword=} {value=}")
+
+
+    return render_template("produkt.html", title=produktname, titel=data['titel'], autor=data['autor'],
+                           stichworte=data['stichworte'], beschreibung=data['beschreibung'], bildlink=data['bildlink'])
+
+
 
 if __name__ == '__main__':
     app.run()
